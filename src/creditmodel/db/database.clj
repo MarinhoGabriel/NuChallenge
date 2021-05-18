9fc24f85cf9f378f0db99fd41a658d21e66d2481
Gabriel Marinho <gabrielvesal@gmail.com>
2021-05-18 17:13:25 -0300

    [#4] - Creating `database.clj` file
    - Creating the file with connection opening and database dropping
    - Ignoring .idea folder

---
+++ src/creditmodel/db/database.clj
@@ -0,0 +1,17 @@
+(ns creditmodel.db.database
+  (:use clojure.pprint)
+  (:require [datomic.api :as datomic]))
+
+; Database URI used to open the connection
+(def uri "datomic:dev//localhost:4334/creditmodel)")
+
+(defn open-connection
+  "Creates the database and connects to it."
+  []
+  (datomic/create-database uri)
+  (datomic/connect uri))
+
+(defn drop-database
+  "Drops the database."
+  []
+  (datomic/delete-database uri))
