8715dde021995877535be44e2b88aa757c32273f
Gabriel Marinho <gabrielvesal@gmail.com>
2021-05-27 17:00:09 -0300

    [#15] - Creating logger module

--- communication/src/br/com/marinho/communication/core.clj
+++
@@ -1,6 +0,0 @@
-(ns br.com.marinho.communication.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
---
+++ logger/src/br/com/marinho/creditmodel/logger/core.clj
@@ -0,0 +1,12 @@
+(ns br.com.marinho.creditmodel.logger.core
+  (:import (java.util.regex Pattern))
+  (:require [br.com.marinho.creditmodel.communication.consumer :as consumer]))
+
+(defn run "Runs the logger service to inform all information about the services running
+  on Kafka Client."
+  []
+  (let [consumer (consumer/create-consumer "br.com.marinho.creditmodel.logger")]
+    (consumer/consume consumer (Pattern/compile "br.com.marinho.creditmodel.*"))))
+
+; Running the service (in loop)
+(run)
